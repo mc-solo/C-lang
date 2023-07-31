a554b12525b6be355b6d8621231f5432ba2cd064
Wondwosen <revatoel@gmial.com>
2023-07-31 13:19:26 +0300

    done with my first linked list code

--- First-steps/linked_list.c
+++ First-steps/linked_list.c
@@ -3,14 +3,14 @@
 
 // definition of a structure for a node
 
-struct Node {
+typedef struct Node {
     int data;
     struct Node *next; //points to the next node
-};
+}Node;
 
 void printList(struct Node* head) //takes a pointer to the hes of the linked list
 {
-    struct Node *current = head; //temporary pointer called current
+    Node *current = head; //temporary pointer called current
     while (current != NULL)
         {
             printf("%d-> ", current->data);
@@ -20,3 +20,32 @@ void printList(struct Node* head) //takes a pointer to the hes of the linked lis
 
 }
 
+int main(void) {
+
+    // creating nodes
+        Node *head = (Node*)malloc(sizeof(Node));
+        Node *second = (Node*)malloc(sizeof(Node));
+        Node *third = (Node*)malloc(sizeof(Node));
+        
+        //assigning data to the node and linking them
+
+        head->data = 1;
+        head->next = second;
+
+        second->data = 2;
+        second->next = third;
+
+        third->data = 3;
+        third->next = NULL;
+
+        //printing the list
+
+        printList(head);
+
+        //freeing the mem
+        free(head);
+        free(second);
+        free(third);
+
+        return (0);
+}
